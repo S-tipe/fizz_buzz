b005066b14bae29077e3cd4489d34df2fdb919ae
Stipe Roguljić <stipe.roguljic@gmail.com>
2022-03-10 16:04:33 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+print("Dobrodošli u fizzbuzz igru !")
+
+end = input("Molimo unesite broj između 1 i 100: ")
+end = int(end)  # convert string into number
+
+for num in range(1, end+1):
+    if num % 3 == 0 and num % 5 == 0:
+        print("fizzbuzz")
+    elif num % 3 == 0:
+        print("fizz")
+    elif num % 5 == 0:
+        print("buzz")
+    else:
+        print(num)
